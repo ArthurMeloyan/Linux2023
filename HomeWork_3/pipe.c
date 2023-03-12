13151d03c63ba2f53fc1af2e2e68b4906ff45532
ArthurMeloyan <meloyan.12@mail.ru>
2023-03-12 15:47:31 +0400

    p1 | p2 | ... | pN

--- HomeWork_3/pipe.c
+++ HomeWork_3/pipe.c
@@ -3,15 +3,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
 int main(int argc, char* argv[])
 {
+    if (argc < 3)
+    {
+        printf("Invalid count of arguments is provided");
+        return 1;
+    }
 
-    int count = argc - 1;
-
-    int fd[count-1][2];
-    pid_t pid[count];
-    for (int i = 0; i < count - 1; ++i)
+    int fd[argc-2][2];
+    pid_t pid[argc-1];
+    for (int i = 0; i < argc - 2; ++i)
     {
         if (pipe(fd[i]) < 0)
         {
@@ -23,28 +25,28 @@ int main(int argc, char* argv[])
     pid[0] = fork();
     if (pid[0] < 0)
     {
-    	perror("Unable to create a child process");
-    	return 1;
+        perror("Unable to create a child process");
+        return 1;
     }
     if (pid[0] == 0)
     {
         close(fd[0][0]);
         dup2(fd[0][1], 1);
         close(fd[0][1]);
-        if(execlp(argv[1], argv[1], NULL) < 0)
+        if (execlp(argv[1], argv[1], NULL) < 0)
         {
-        	perror("Execution failed");
-        	return 1;
+            perror("Execution failed");
+            return 1;
         }
     }
 
-    for (int i = 1; i < count; ++i)
+    for (int i = 1; i < argc - 2; ++i)
     {
         pid[i] = fork();
         if (pid[i] < 0)
         {
-        	perror("Forking failed");
-        	return 1;
+            perror("Cannot create a child process");
+            return 1;
         }
         if (pid[i] == 0)
         {
@@ -53,25 +55,58 @@ int main(int argc, char* argv[])
             close(fd[i-1][0]);
 
             close(fd[i][0]);
-            dup2(fd[i][1], 1);
+            dup2(fd[i][1],1);
             close(fd[i][1]);
+             for (int j = 0; j < argc - 2; ++j)
+            {
+                if (j != i && j != i - 1)
+                {
+                    close(fd[j][0]);
+                    close(fd[j][1]);
+                }
+            }
             if (execlp(argv[i+1], argv[i+1], NULL) < 0)
             {
-            	perror("Execution failed");
-            	return 1;
+                perror("Execution failed");
+                return 1;
             }
         }
     }
-    for (int i = 0; i < count - 1; ++i)
+
+  
+
+    pid[argc-2] = fork();
+    if (pid[argc-2] < 0)
+    {
+        perror("Unable create a child process");
+        return 1;
+    }
+    if (pid[argc-2] == 0)
+    {
+        for (int i = 0; i < argc - 3; ++i)
+        {
+            close(fd[i][0]);
+            close(fd[i][1]);
+        }
+        
+        close(fd[argc-3][1]);
+        dup2(fd[argc-3][0], 0);
+        close(fd[argc-3][0]);
+        if (execlp(argv[argc-1], argv[argc-1], NULL) < 0)
+        {
+            perror("Execution failed");
+            return 1;
+        }
+    }
+    for (int i = 0; i < argc - 2; ++i)
     {
         close(fd[i][0]);
         close(fd[i][1]);
     }
 
-    for (int i = 0; i < count; ++i)
+    for (int i = 0; i < argc - 1; ++i)
     {
         waitpid(pid[i], NULL, 0);
     }
     return 0;
-
 }
