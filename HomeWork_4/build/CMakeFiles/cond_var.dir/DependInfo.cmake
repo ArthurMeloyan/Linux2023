d2b6d32ee1291f3b680b6fa5f19c1abb473c2116
ArthurMeloyan <117680986+ArthurMeloyan@users.noreply.github.com>
2023-04-16 12:54:36 +0400

    Add files via upload
    conditional variable

---
+++ HomeWork_4/build/CMakeFiles/cond_var.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/arthur/Desktop/Linux2023/HomeWork_4/main.c" "CMakeFiles/cond_var.dir/main.c.o" "gcc" "CMakeFiles/cond_var.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/arthur/Desktop/Linux2023/HomeWork_4/build/CMakeFiles/queue.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ HomeWork_4/build/CMakeFiles/cond_var.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cond_var.dir/main.c.o"
+  "CMakeFiles/cond_var.dir/main.c.o.d"
+  "cond_var"
+  "cond_var.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cond_var.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
